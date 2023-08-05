f1fb8571d9da105edaf4fa64cd9cc45498dcdc39
dahenr <davidvh@gmail.com>
2023-08-05 16:44:40 -0700

    Stalwart fixes

--- infra/caddy_to_letsencrypt.sh
+++ infra/caddy_to_letsencrypt.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-while 1
+set -x -e
+while [ 1 ]
 do
 
 # Get the path to the certs
-SOURCE=`find /var/lib/docker/volumes/infra_certs/_data/ | grep .crt | xargs ls -t | grep $URI --max-count=1 | xargs dirname`
+SOURCE=`find /certs/ | grep .crt | xargs ls -t | grep $URI -m 1 | xargs dirname`
 
-if $SOURCE
+if [ $SOURCE ]
 then
     # Copy to the expected location for Stalwart Mail Server
+    mkdir -p /mail/etc/certs/$URI/
     cp --update $SOURCE/$URI.crt /mail/etc/certs/$URI/fullchain.pem
     cp --update $SOURCE/$URI.key /mail/etc/certs/$URI/privkey.pem
 fi
