57d43771ce57bd7e71efc37114b812801d2cbd4e
dahenr <davidvh@gmail.com>
2023-08-05 19:26:15 -0700

    Fix cert copy

--- infra/caddy_to_letsencrypt.sh
+++ infra/caddy_to_letsencrypt.sh
@@ -10,9 +10,9 @@ SOURCE=`find /certs/ | grep .crt | xargs ls -t | grep $URI -m 1 | xargs dirname`
 if [ $SOURCE ]
 then
     # Copy to the expected location for Stalwart Mail Server
-    mkdir -p /mail/etc/certs/$URI/
-    cp --update $SOURCE/$URI.crt $OUTDIR/$URI/fullchain.pem
-    cp --update $SOURCE/$URI.key $OUTDIR/$URI/privkey.pem
+    mkdir -p $OUTDIR
+    cp --update $SOURCE/$URI.crt $OUTDIR/fullchain.pem
+    cp --update $SOURCE/$URI.key $OUTDIR/privkey.pem
 fi
 
 sleep 300
